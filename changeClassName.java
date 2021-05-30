0769c3052e2d7c8c0ad445dc393283640e92b42f
QinisoNdovela <85028852+QinisoNdovela@users.noreply.github.com>
2021-05-30 12:19:53 +0200

    Add files via upload

---
+++ changeClassName.java
@@ -0,0 +1,13 @@
+/*
+   Ndovela Qiniso
+   217182879
+   Java Programming assignment 2
+*/
+
+import javax.swing.*;
+public class changeClassName {
+    public static void main(String[] args) {
+    JOptionPane.showMessageDialog(null, "Programming is fun",null, JOptionPane.INFORMATION_MESSAGE);
+     System.exit(0);
+    } 
+}
